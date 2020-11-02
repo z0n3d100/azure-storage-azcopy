606a2ed405e6818d33975cf70d6b4c2d25e79112
Narasimha Kulkarni <63087328+nakulkar-msft@users.noreply.github.com>
2020-11-02 11:53:07 +0530

    Restrict PutBlobFromURL to gcp sources

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.6.102-preview"
+const AzcopyVersion = "10.6.103-preview"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const GCPImportUserAgent = "GCPImport " + UserAgent
